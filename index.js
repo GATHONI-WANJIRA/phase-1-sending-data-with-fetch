ddff90a0018363cbd0f2d1479fa406bd3d2250ce
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-26 22:59:11 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+// fetch("http://localhost:3000/dogs")
+// .then(function(res){
+//     return(res.json())
+// })
+// .then(function(ourData){
+//     console.log(ourData)
+// })
+
+
+  function submitData (userName , userEmail){
+    const data = {
+    name: userName,
+    email: userEmail,
+        
+    }
+    
+   const postObject = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-type":"application/json",
+            accept:"application/json"
+        }
+    
+    }
+    return fetch("http://localhost:3000/users" , postObject)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function(data) {
+        // return data;
+        document.querySelector("body").textContent = data.id 
+    })
+    .catch((err) => {
+        document.querySelector("body").textContent = err.message;
+      });
+
+}
